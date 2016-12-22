b2c1a83e6ccb6610f3282ba64f4380be19d42e12
cwilliams3579 <clevelandwilliams3579@gmail.com>
2016-12-22 11:48:41 -0600

    added validation to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-  validates
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+  validates :username, presence: true, length: { in: 2..20 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+  before_save do
+    self.email.downcase!
+  end
 end
