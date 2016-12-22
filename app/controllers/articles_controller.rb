9261707aaa0ee4e7f97c300a85b9c0de62d281d3
cwilliams3579 <clevelandwilliams3579@gmail.com>
2016-12-22 10:36:35 -0600

    styled articles template

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
   def update
 
     if @article.update(article_params)
-      flash[:success] = "Article was successfully updated"
+      flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
