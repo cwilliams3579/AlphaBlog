8ec445a92ab4937395bfe65f90b1073b6e1b62c4
cwilliams3579 <clevelandwilliams3579@gmail.com>
2016-12-23 01:05:04 -0600

    added admin user functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-  has_many :articles
+  has_many :articles, dependent: :destroy
   EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { in: 3..25 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
